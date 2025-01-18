6bc2761875546da5006d41aab348287045d4a411
Aniket <aniketkalawat88@gmail.com>
2025-01-18 17:59:22 +0530

    get-delete-review

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,6 +2,7 @@ const Product = require("../models/productModel");
 const ErrorHander = require("../utils/errorHandler");
 const catchAsyncError = require("../middleware/catchAsyncError");
 const ApiFeatures = require("../utils/apifeatures");
+const { query } = require("express");
 
 // Create Product - Admin
 exports.createProduct = catchAsyncError(async (req, res, next) => {
@@ -80,13 +81,10 @@ exports.updateProduct = catchAsyncError(async (req, res , next) => {
 })
 
 // Delete Product - Admin
-exports.deleteProduct = catchAsyncError(async (req, res) => {
+exports.deleteProduct = catchAsyncError(async (req, res , next) => {
   let product = await Product.findById(req.params.id);
   if(!product){
-    return res.status(500).json({
-      success: false,
-      message: 'Product not found'
-    })
+    return next(new ErrorHander("Product not found" , 404))
   }
   await product.deleteOne();
   res.status(200).json({
@@ -95,6 +93,97 @@ exports.deleteProduct = catchAsyncError(async (req, res) => {
   })
 })
 
+// Create new Review or Update the review
+exports.createProductReview = catchAsyncError(async (req, res , next) => {
 
-// create new review or update the review
+  const { rating , comment , productId} = req.body;
+  
+  const review = {
+    user:req.user._id,
+    name:req.user.name,
+    rating:Number(rating),
+    comment,
+  }
+  const product = await Product.findById(productId);
 
+  const isReviewed = product.reviews.find(rev => rev.user.toString() === req.user._id.toString())
+
+  // review phle hua h ki nhi particular user ka 
+  if(isReviewed){
+    product.reviews.forEach(rev => {
+      if(rev.user.toString() === req.user._id.toString())
+        (rev.rating = rating),(rev.comment = comment)
+    })
+  }
+  else{
+    product.reviews.push(review)
+    product.numOfReviews = product.reviews.length
+  }
+
+  
+  // yeh overroll rating h mean avarge rating nikalna
+  //  4+5+5+2  = 16/4 = 4 
+  let avg = 0;
+
+  product.reviews.forEach((rev)=> {
+    avg += rev.rating                   // avg = avg + rev.rating
+  });
+
+  product.ratings = avg / product.reviews.length;
+
+  await product.save({ validateBeforeSave : false});
+
+  res.status(200).json({
+    success:true,
+  })
+})
+
+// Get All reviews of a product
+exports.getProductReviews = catchAsyncError(async ( req, res, next) => {
+  const product = await Product.findById(req.query.id);
+
+  if(!product){
+    return next(new ErrorHander("Product not found" , 404))
+  }
+  res.status(200).json({
+    success:true,
+    reviews: product.reviews
+  })
+})
+
+
+// Delete Review
+exports.deleteReview = catchAsyncError(async ( req, res, next) => {
+  const product = await Product.findById(req.query.productId);
+
+  if(!product){
+    return next(new ErrorHander("Product not found" , 404))
+  }
+
+  const reviews = product.reviews.filter( rev => rev._id.toString() !== req.query.id.toString())
+  
+  let avg = 0;
+
+  reviews.forEach((rev)=> {
+    avg += rev.rating                   // avg = avg + rev.rating
+  });
+
+  const ratings = avg / reviews.length;
+
+  const numOfReviews = reviews.length;
+
+  await Product.findByIdAndUpdate(req.query.productId , {
+    reviews,
+    ratings,
+    numOfReviews,
+  },{
+    new:true,
+    runValidators:true,
+    useFindAndModify : false
+  })
+
+  res.status(200).json({
+    success:true,
+    // reviews: product.reviews
+  })
+})
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getAllProducts, createProduct, updateProduct, deleteProduct, getProductDetails } = require('../controllers/productController');
+const { getAllProducts, createProduct, updateProduct, deleteProduct, getProductDetails, createProductReview, getProductReviews, deleteReview } = require('../controllers/productController');
 const { isAuthenticatedUser , authorizeRoles } = require('../middleware/auth');
 
 const router = express.Router();
@@ -9,6 +9,10 @@ router.route("/product/new").post(isAuthenticatedUser , authorizeRoles("admin") 
 router.route("/product/:id").put( isAuthenticatedUser ,authorizeRoles("admin") , updateProduct);
 router.route("/product/:id").delete(isAuthenticatedUser ,authorizeRoles("admin") , deleteProduct);
 router.route("/product/:id").get(getProductDetails);
+
+router.route("/review").put(isAuthenticatedUser , createProductReview)
+
+router.route("/reviews").get(getProductReviews).delete(isAuthenticatedUser , deleteReview)
 
 
 module.exports = router;
